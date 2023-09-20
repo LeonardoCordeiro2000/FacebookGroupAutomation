568c1ed397227e89ee79a95bf5552577c0f926d1
LeonardoCordeiro2000 <leocordeiro09@gmail.com>
2023-09-20 01:57:49 -0300

    Changing variables name

--- CoockieClickerBot/CoockieClickerBot.py
+++ CoockieClickerBot/CoockieClickerBot.py
@@ -9,9 +9,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 service = Service(ChromeDriverManager().install())
 options = webdriver.ChromeOptions()
 options.add_argument("--disable-notifications")
-navegador = webdriver.Chrome(options = options,service = service)
+driver = webdriver.Chrome(options = options,service = service)
 
-navegador.implicitly_wait(60);
+driver.implicitly_wait(60);
 
 #Mudar variaveis de metodos para campos da classe FacebookBot
 #Mudar o nome das variaveis para ingles
@@ -22,10 +22,10 @@ class FacebookBot():
     def login(self):
 
         loginPage = 'https://www.facebook.com/'
-        navegador.get(loginPage)
-        emailBox = navegador.find_element("xpath",'//*[@id="email"]')
-        passwordBox = navegador.find_element("xpath", '//*[@id="pass"]')
-        loginButton = navegador.find_element("xpath", "//button[text()='Entrar']")
+        driver.get(loginPage)
+        emailBox = driver.find_element("xpath",'//*[@id="email"]')
+        passwordBox = driver.find_element("xpath", '//*[@id="pass"]')
+        loginButton = driver.find_element("xpath", "//button[text()='Entrar']")
         email = 'YourEmail'
         password = 'YourPass'
 
@@ -33,25 +33,25 @@ class FacebookBot():
         passwordBox.send_keys(password);
 
         loginButton.click();
-        navegador.maximize_window();
+        driver.maximize_window();
     
         self.Posting()
 
 
     def Posting(self):
 
-        grupo = "//span[text()='League of Legends - Brasil']"
-        botaoEscrever = "//span[text()='Escreva algo...']"
+        group = "//span[text()='League of Legends - Brasil']"
+        writeButton = "//span[text()='Escreva algo...']"
         textBoxFacebook = "//br[@data-text='true']"
         publishButton = "//span[text()='Publicar']"
 
-        navegador.find_element("xpath", grupo).click();
+        driver.find_element("xpath", group).click();
 
-        navegador.find_element("xpath", botaoEscrever).click();
+        driver.find_element("xpath", writeButton).click();
 
-        navegador.find_element("xpath", textBoxFacebook).send_keys("Mono sup gold, procuro duo pra subir de elo junto e jogar aos finais de semana, alguem?");
+        driver.find_element("xpath", textBoxFacebook).send_keys("Mono sup gold, procuro duo pra subir de elo junto e jogar aos finais de semana, alguem?");
 
-        navegador.find_element("xpath", publishButton).click();
+        driver.find_element("xpath", publishButton).click();
 
         time.sleep(5);
 
